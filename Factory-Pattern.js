8e03ceee39596accca1f88a61d6b3d2b4c7eb0e7
mlwebdev <marvinllbiz@gmail.com>
2017-08-13 12:25:43 -0400

    Add JS Design Patterns

---
+++ Factory-Pattern.js
@@ -0,0 +1,15 @@
+// Factory-Pattern.js
+var CarFactory = function ( year, paint, make, model ) {
+	var car = {};
+	car.year = year;
+	car.paint = paint;
+	car.make = make;
+	car.model = model;
+/* Function that output properties */
+	car.showCar = function() {
+		console.log( `${this.year} ${this.paint} ${this.make} ${this.model}` );
+	}
+	return car;
+}
+var car1 = CarFactory( "2017", "Black", "Lincoln", "Continental");
+car1.showCar();
---
+++ Prototype-Pattern.js
@@ -0,0 +1,18 @@
+// Prototype-Pattern.js
+var carProto = function () {	
+}
+
+carProto.prototype.year = "";
+carProto.prototype.paint = "";
+carProto.prototype.make = "";
+carProto.prototype.model = "";
+
+carProto.prototype.showCar = function() {
+	console.log( `${this.year} ${this.paint} ${this.make} ${this.model}` );
+}   
+var car1 = new carProto();
+car1.year = "2017";
+car1.paint = "Black";
+car1.make = "Lincoln";
+car1.model = "Continental";
+car1.showCar();
