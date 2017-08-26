6b99e9b35d962153d354a29dc1ed516950d0ae0d
mlwebdev <marvinllbiz@gmail.com>
2017-08-26 16:44:58 -0400

    Update Dynamic-Prototype-Pattern.js

--- Dynamic-Prototype-Pattern.js
+++ Dynamic-Prototype-Pattern.js
@@ -1,15 +1,15 @@
 // Dynamic-Prototype-Pattern.js
-var carDynamicProto = function (year, paint, make, model) {	
-	this.year = year;
-	this.paint = paint;
-	this.make = make;
-	this.model = model;
+var carDynamicProto = function(year, paint, make, model) {
+  this.year = year;
+  this.paint = paint;
+  this.make = make;
+  this.model = model;
 
-	if ( typeof this.showCar != 'function') {
-		carDynamicProto.prototype.showCar = function() {
-			console.log( `${this.year} ${this.paint} ${this.make} ${this.model}` );
-		}   
-	}
-};   
+  if (typeof this.showCar != 'function') {
+    carDynamicProto.prototype.showCar = function() {
+      console.log(`${this.year} ${this.paint} ${this.make} ${this.model}`);
+    }
+  }
+};
 var car1 = new carDynamicProto("2017", "Black", "Lincoln", "Continental");
 car1.showCar();
