e05efdd3991d08283539c35d08bc40e6ad5548e7
mlwebdev <marvinllbiz@gmail.com>
2017-08-26 16:43:41 -0400

    Update Constructor-Pattern.js

--- Constructor-Pattern.js
+++ Constructor-Pattern.js
@@ -1,13 +1,13 @@
 // Constructor-Pattern.js
-var CarConstructor = function ( year, paint, make, model ) {
-	this.year = year;
-	this.paint = paint;
-	this.make = make;
-	this.model = model;
-/* Function that output properties */
-	this.showCar = function() {
-		console.log( `${this.year} ${this.paint} ${this.make} ${this.model}` );
-	}
+var CarConstructor = function(year, paint, make, model) {
+  this.year = year;
+  this.paint = paint;
+  this.make = make;
+  this.model = model;
+  /* Function that output properties */
+  this.showCar = function() {
+    console.log(`${this.year} ${this.paint} ${this.make} ${this.model}`);
+  }
 }
-var car1 = new CarConstructor( "2017", "Black", "Lincoln", "Continental");
+var car1 = new CarConstructor("2017", "Black", "Lincoln", "Continental");
 car1.showCar();
